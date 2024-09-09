646427dadc95e2a54a90878dede1f4a4509ec4f2
no-im-bk <80921911+no-im-bk@users.noreply.github.com>
2024-09-08 19:23:59 -0600

    added diffuse5

--- src/color copy.h
+++ src/color copy.h
@@ -9,22 +9,22 @@
 using color = vec3;
 
 inline double linear_to_gamma(double linear_component) {
-    if (linear_component > 0) {
-        return std::sqrt(linear_component);
-    } else {
-        return 0;
-    }
+    // if (linear_component > 0) {
+    //     return std::sqrt(linear_component);
+    // } else {
+    //     return 0;
+    // }
+    return linear_component;
 }
 
 void write_color(std::ostream& out, const color& pColor) {
     static const interval intensity(0.000, 0.999);
 
-    
-
     // gamma transform
     auto r = linear_to_gamma(pColor.x());
     auto g = linear_to_gamma(pColor.y());
-    auto b = linear_to_gamma(pColor.z());
+    auto b = linear_to_gamma(pColor.x());
+
 
 
     // convert 0-1 values to colors
