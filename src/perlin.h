981b622bed1297f1bfc0624587ddf03e29729a56
no-im-bk <80921911+no-im-bk@users.noreply.github.com>
2024-09-28 14:31:14 -0600

    added perlin_texture

--- src/perlin.h
+++ src/perlin.h
@@ -6,7 +6,7 @@ class perlin {
     public:
         perlin() {
             for(int i = 0; i < point_count; i++) {
-                randfloat[i] = random_double();
+                randvec[i] = unit_vector(vec3::random(-1,1));
             }
 
             perlin_generate_perm(perm_x);
@@ -19,31 +19,31 @@ class perlin {
             auto y = int(std::floor(p.y()));
             auto z = int(std::floor(p.z()));
 
-            auto xx = p.x() - std::floor(p.x());
-            auto yy = p.y() - std::floor(p.y());
-            auto zz = p.z() - std::floor(p.z());
+            double x_fraction = p.x() - std::floor(p.x());
+            double y_fraction = p.y() - std::floor(p.y());
+            double z_fraction = p.z() - std::floor(p.z());
 
-            xx = xx*xx*(-2*xx + 3);
-            yy = yy*yy*(-2*yy + 3);
-            zz = zz*zz*(-2*zz + 3);
+            double xx = x_fraction*x_fraction*(-2*x_fraction + 3);
+            double yy = y_fraction*y_fraction*(-2*y_fraction + 3);
+            double zz = z_fraction*z_fraction*(-2*z_fraction + 3);
 
             double value = 0;
 
-            value += randfloat[perm_x[ x    & 255] ^ perm_y[ y    & 255] ^ perm_z[ z    & 255]] * (1-xx) * (1-yy) * (1-zz);
-            value += randfloat[perm_x[ x    & 255] ^ perm_y[ y    & 255] ^ perm_z[(z+1) & 255]] * (1-xx) * (1-yy) *    zz ;
-            value += randfloat[perm_x[ x    & 255] ^ perm_y[(y+1) & 255] ^ perm_z[ z    & 255]] * (1-xx) *    yy  * (1-zz);
-            value += randfloat[perm_x[ x    & 255] ^ perm_y[(y+1) & 255] ^ perm_z[(z+1) & 255]] * (1-xx) *    yy  *    zz ;
-            value += randfloat[perm_x[(x+1) & 255] ^ perm_y[ y    & 255] ^ perm_z[ z    & 255]] *    xx  * (1-yy) * (1-zz);
-            value += randfloat[perm_x[(x+1) & 255] ^ perm_y[ y    & 255] ^ perm_z[(z+1) & 255]] *    xx  * (1-yy) *    zz ;
-            value += randfloat[perm_x[(x+1) & 255] ^ perm_y[(y+1) & 255] ^ perm_z[ z    & 255]] *    xx  *    yy  * (1-zz);
-            value += randfloat[perm_x[(x+1) & 255] ^ perm_y[(y+1) & 255] ^ perm_z[(z+1) & 255]] *    xx  *    yy  *    zz;
+            value += dot(randvec[perm_x[ x    & 255] ^ perm_y[ y    & 255] ^ perm_z[ z    & 255]], vec3(x_fraction  ,y_fraction  ,z_fraction  )) * (1-xx) * (1-yy) * (1-zz);
+            value += dot(randvec[perm_x[ x    & 255] ^ perm_y[ y    & 255] ^ perm_z[(z+1) & 255]], vec3(x_fraction  ,y_fraction  ,z_fraction-1)) * (1-xx) * (1-yy) *    zz ;
+            value += dot(randvec[perm_x[ x    & 255] ^ perm_y[(y+1) & 255] ^ perm_z[ z    & 255]], vec3(x_fraction  ,y_fraction-1,z_fraction  )) * (1-xx) *    yy  * (1-zz);
+            value += dot(randvec[perm_x[ x    & 255] ^ perm_y[(y+1) & 255] ^ perm_z[(z+1) & 255]], vec3(x_fraction  ,y_fraction-1,z_fraction-1)) * (1-xx) *    yy  *    zz ;
+            value += dot(randvec[perm_x[(x+1) & 255] ^ perm_y[ y    & 255] ^ perm_z[ z    & 255]], vec3(x_fraction-1,y_fraction  ,z_fraction  )) *    xx  * (1-yy) * (1-zz);
+            value += dot(randvec[perm_x[(x+1) & 255] ^ perm_y[ y    & 255] ^ perm_z[(z+1) & 255]], vec3(x_fraction-1,y_fraction  ,z_fraction-1)) *    xx  * (1-yy) *    zz ;
+            value += dot(randvec[perm_x[(x+1) & 255] ^ perm_y[(y+1) & 255] ^ perm_z[ z    & 255]], vec3(x_fraction-1,y_fraction-1,z_fraction  )) *    xx  *    yy  * (1-zz);
+            value += dot(randvec[perm_x[(x+1) & 255] ^ perm_y[(y+1) & 255] ^ perm_z[(z+1) & 255]], vec3(x_fraction-1,y_fraction-1,z_fraction-1)) *    xx  *    yy  *    zz ;
 
             return value;
         }
 
     private:
         static const int point_count = 256;
-        double randfloat[point_count];
+        vec3 randvec[point_count];
         int perm_x[point_count];
         int perm_y[point_count];
         int perm_z[point_count];
